2f76439e730099bae96d1f2ec02336a6fcd89c43
sglee0323 <sglee@gmail.com>
2020-09-10 01:21:53 +0000

    delete build

--- build/lib/src/phy/common/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sugi/srsLTE/lib/src/phy/common/test
-# Build directory: /home/sugi/srsLTE/build/lib/src/phy/common/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(sequence_test "sequence_test")
